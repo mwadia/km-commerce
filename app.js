7df1d4c30f5193e513fbe863bcef0bccea7cb515
mohammadalwadia@gmail.com <54046997+mwadia@users.noreply.github.com>
2022-11-14 11:47:43 +0200

    hello

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const app=express();
-const compression = require("compression");
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const compression = require('compression');
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
-const port =process.env.PORT||5000
+const port = process.env.PORT || 5000;
 app.set('port', port);
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
 
-app.use('/user',(req,res,next)=>{
-  if(req.user){
-    res.json(req.user)
-  }else{
-    res.json({msg:"user is not"})
-  }
-  next()
-})
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-module.exports=app
+app.use('/user', (req, res, next) => {
+  if (req.user) {
+    res.json(req.user);
+  } else {
+    res.json({ msg: 'user is not' });
+  }
+  next();
+});
+
+module.exports = app;
