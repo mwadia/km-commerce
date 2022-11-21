0be5c26e14b1483679b9dfd6f143255515422043
dev-mohmmed-alshorafa <mohmmedalshorafa1996@gmail.com>
2022-11-21 17:59:58 +0200

    test

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -17,10 +17,10 @@ const jwtFun = (info, res) => {
 };
 const auth = (req, res, next) => {
   let token
-  console.log(req.header('Authorization'));
+  console.log(req.header('Authorization'),111111111);
 if(req.header('Authorization')){
    token  = req.header('Authorization').split(' ')[1];
-
+console.log(222222);
 }
   jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
     if (!err) {
